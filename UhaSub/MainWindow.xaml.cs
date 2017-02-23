8ea30ad96bb428bdfa55ac07b8cb2c2cc1f8f993
xue-blood <xue.blood@outlook.com>
2017-02-24 00:00:53 +0800

    make some basic work.

---
+++ UhaSub/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using Unosquare;
+
+namespace UhaSub
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DispatcherTimer dt = new DispatcherTimer();
+            dt.Interval = TimeSpan.FromMilliseconds(16);
+            dt.Tick += new EventHandler(OnProcessViewports);
+            dt.Start();
+        }
+
+
+      
+        DateTime old;
+        
+        private void OnProcessViewports(object sender, EventArgs e)
+        {
+            //timeText.Text = DateTime.Now.ToString("HH:mm:ss.fff");
+            DateTime n = DateTime.Now;
+            double delta = (n - old).TotalMilliseconds;
+            old = n;
+
+            psText.Text = video.Position.ToString();
+
+            //time.Value = (double)video.Position;
+            
+        }
+
+
+        
+    
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Right: video.Pause(); video.Position += 10; video.Play(); break;
+                case Key.Left:  video.Pause(); video.Position -= 10; video.Play(); break;
+            }
+        }
+
+        private void OnOpenFile(object sender, RoutedEventArgs e)
+        {
+            var fileDialog = new OpenFileDialog();
+            fileDialog.Filter =
+                "Video files (*.mp4)|*.mp4;*.mkv|All files (*.*)|*.*";
+
+            if (fileDialog.ShowDialog() == true)
+            {
+                video.Source = new Uri(fileDialog.FileName);
+                video.Play();
+            }
+        }
+    }
+}
---
+++ UhaSub/Video.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UhaSub
+{
+    /// <summary>
+    /// Interaction logic for Video.xaml
+    /// </summary>
+    public partial class Video : UserControl
+    {
+        public Video()
+        {
+            InitializeComponent();
+        }
+
+        bool Ex = true;
+
+        bool useEx(string uri)
+        {
+            if (uri.Contains("mkv"))
+            {
+                Ex = true;
+                mePlayerex.Visibility = Visibility.Visible;
+                return true;
+            }
+            Ex = false;
+            return false;
+        }
+
+
+        public Uri Source 
+        {
+            get
+            {
+                if (Ex)
+                {
+                    return mePlayerex.Source;
+                }
+                return mePlayer.Source;
+            }
+            set 
+            {
+                if (useEx(value.OriginalString)) 
+                    mePlayerex.Source = value;
+                else 
+                    mePlayer.Source = value;
+
+            } 
+        }
+
+        public decimal Position
+        {
+            get
+            {
+                if (Ex)
+                {
+                    return mePlayerex.Position;
+                }
+                return (decimal)mePlayer.Position.TotalSeconds;
+            }
+            set
+            {
+                if (Ex)
+                    mePlayerex.Position = value;
+                else
+                    mePlayer.Position = TimeSpan.FromSeconds((double)value);
+
+            }
+        }
+
+        public void Play()
+        {
+            if (Ex) 
+                mePlayerex.Play();
+            else
+                mePlayer.Play();
+
+        }
+
+        public void Pause()
+        {
+            if (Ex)
+                mePlayerex.Pause();
+            else
+                mePlayer.Pause();
+
+        }
+    }
+}
