083747231fe47f2e760764d6ce613aef9b2fc1c7
xue-blood <xue.blood@outlook.com>
2017-02-25 12:10:49 +0800

    chage video backend to vlc; update gpl-3 license.

--- UhaSub/MainWindow.xaml.cs
+++ UhaSub/MainWindow.xaml.cs
@@ -18,7 +18,6 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
-using Unosquare;
 
 namespace UhaSub
 {
@@ -39,10 +38,8 @@ namespace UhaSub
             dt.Start();
         }
 
-
-      
         DateTime old;
-        
+
         private void OnProcessViewports(object sender, EventArgs e)
         {
             //timeText.Text = DateTime.Now.ToString("HH:mm:ss.fff");
@@ -50,35 +47,35 @@ namespace UhaSub
             double delta = (n - old).TotalMilliseconds;
             old = n;
 
-            psText.Text = video.Position.ToString();
+            //psText.Text = video.Position.ToString();
 
             //time.Value = (double)video.Position;
-            
+
         }
 
-
-        
     
         private void Window_KeyDown(object sender, KeyEventArgs e)
         {
             switch (e.Key)
             {
-                case Key.Right: video.Pause(); video.Position += 10; video.Play(); break;
-                case Key.Left:  video.Pause(); video.Position -= 10; video.Play(); break;
+                //case Key.Right: video.Pause(); video.Position += 10; video.Play(); break;
+                //case Key.Left:  video.Pause(); video.Position -= 10; video.Play(); break;
             }
         }
 
         private void OnOpenFile(object sender, RoutedEventArgs e)
         {
+
             var fileDialog = new OpenFileDialog();
             fileDialog.Filter =
                 "Video files (*.mp4)|*.mp4;*.mkv|All files (*.*)|*.*";
 
             if (fileDialog.ShowDialog() == true)
             {
-                video.Source = new Uri(fileDialog.FileName);
-                video.Play();
+                video.Source = new Uri(fileDialog.FileName);    
+                //video.Play();
             }
         }
+
     }
 }
--- UhaSub/Video.xaml.cs
+++ UhaSub/Video.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -15,6 +16,18 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
+/*
+ * using for nVlc
+ */
+using Declarations;
+using Declarations.Events;
+using Declarations.Media;
+using Declarations.Players;
+using Implementation;
+using Microsoft.Win32;
+using System.Windows.Controls.Primitives;
+
 namespace UhaSub
 {
     /// <summary>
@@ -25,79 +38,160 @@ namespace UhaSub
         public Video()
         {
             InitializeComponent();
+
+            LoadVlc();
+
         }
 
-        bool Ex = true;
+        // varial for nVlc
+        IMediaPlayerFactory m_factory;
+        public IVideoPlayer m_player;
+        public IMedia m_media;
 
-        bool useEx(string uri)
+        private volatile bool m_isDrag;
+
+
+        void LoadVlc()
         {
-            if (uri.Contains("mkv"))
-            {
-                Ex = true;
-                mePlayerex.Visibility = Visibility.Visible;
-                return true;
-            }
-            Ex = false;
-            return false;
+            System.Windows.Forms.Panel p = new System.Windows.Forms.Panel();
+            p.BackColor = System.Drawing.Color.FromArgb(33,33,33);
+            windowsFormsHost1.Child = p;
+
+            m_factory = new MediaPlayerFactory();
+            m_player = m_factory.CreatePlayer<IVideoPlayer>();
+
+
+            m_player.Events.PlayerPositionChanged += new EventHandler<MediaPlayerPositionChanged>(
+              Events_PlayerPositionChanged);
+            m_player.Events.TimeChanged += new EventHandler<MediaPlayerTimeChanged>(
+                Events_TimeChanged);
+            m_player.Events.MediaEnded += new EventHandler(
+              Events_MediaEnded);
+            m_player.Events.PlayerStopped += new EventHandler(
+              Events_PlayerStopped);
+
+            m_player.WindowHandle = p.Handle;
         }
 
-
-        public Uri Source 
+        void Events_PlayerStopped(object sender, EventArgs e)
         {
-            get
+            this.Dispatcher.BeginInvoke(new Action(delegate
             {
-                if (Ex)
+                InitControls();
+            }));
+        }
+
+        void Events_MediaEnded(object sender, EventArgs e)
+        {
+            this.Dispatcher.BeginInvoke(new Action(delegate
+            {
+                InitControls();
+            }));
+        }
+
+        private void InitControls()
+        {
+            //slider1.Value = 0;
+            //label1.Content = "00:00:00";
+            //label3.Content = "00:00:00";
+        }
+
+        void Events_TimeChanged(object sender, MediaPlayerTimeChanged e)
+        {
+            this.Dispatcher.BeginInvoke(new Action(delegate
+            {
+                //label1.Content = TimeSpan.FromMilliseconds(e.NewTime).ToString().Substring(0, 8);
+            }));
+        }
+
+        void Events_PlayerPositionChanged(object sender, MediaPlayerPositionChanged e)
+        {
+            this.Dispatcher.BeginInvoke(new Action(delegate
+            {
+                if (!m_isDrag)
                 {
-                    return mePlayerex.Source;
+                   // slider1.Value = (double)e.NewPosition;
                 }
-                return mePlayer.Source;
-            }
-            set 
-            {
-                if (useEx(value.OriginalString)) 
-                    mePlayerex.Source = value;
-                else 
-                    mePlayer.Source = value;
-
-            } 
+            }));
         }
 
-        public decimal Position
+        private void OpenMedia()
         {
-            get
-            {
-                if (Ex)
-                {
-                    return mePlayerex.Position;
-                }
-                return (decimal)mePlayer.Position.TotalSeconds;
-            }
-            set
-            {
-                if (Ex)
-                    mePlayerex.Position = value;
-                else
-                    mePlayer.Position = TimeSpan.FromSeconds((double)value);
+                m_media = m_factory.CreateMedia<IMediaFromFile>(source.OriginalString);
 
-            }
+                m_media.Events.DurationChanged += new EventHandler<MediaDurationChange>(
+                    Events_DurationChanged);
+                m_media.Events.StateChanged += new EventHandler<MediaStateChange>(
+                    Events_StateChanged);
+
+                m_player.Open(m_media);
+                m_player.Play();
+            
         }
 
-        public void Play()
+        private void button3_Click(object sender, RoutedEventArgs e)
         {
-            if (Ex) 
-                mePlayerex.Play();
-            else
-                mePlayer.Play();
-
+            m_player.Play();
         }
 
-        public void Pause()
+        void Events_StateChanged(object sender, MediaStateChange e)
         {
-            if (Ex)
-                mePlayerex.Pause();
-            else
-                mePlayer.Pause();
+            this.Dispatcher.BeginInvoke(new Action(delegate
+            {
 
+            }));
         }
+
+        void Events_DurationChanged(object sender, MediaDurationChange e)
+        {
+            this.Dispatcher.BeginInvoke(new Action(delegate
+            {
+                //label3.Content = TimeSpan.FromMilliseconds(e.NewDuration).ToString().Substring(0, 8);
+            }));
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            m_player.Pause();
+        }
+
+        private void button4_Click(object sender, RoutedEventArgs e)
+        {
+            m_player.Stop();
+        }
+
+        private void button5_Click(object sender, RoutedEventArgs e)
+        {
+            m_player.ToggleMute();
+        }
+
+        private void slider2_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (m_player != null)
+            {
+                m_player.Volume = (int)e.NewValue;
+            }
+        }
+
+        private void slider1_DragCompleted(object sender, DragCompletedEventArgs e)
+        {
+            //m_player.Position = (float)slider1.Value;
+            m_isDrag = false;
+        }
+
+        private void slider1_DragStarted(object sender, DragStartedEventArgs e)
+        {
+            m_isDrag = true;
+        }
+
+        private Uri source;
+        public Uri Source
+        {
+            get { return source; }
+            set { source = value; OpenMedia(); }
+        }
+
+
     }
+
 }
